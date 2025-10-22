f43dd662166127a947cab22734b2ab26b8d07916
ivsjsc <ivscorp.vn@gmail.com>
2025-10-22 11:30:38 +0700

    commit

--- webapp/src/components/Assistant.tsx
+++
@@ -1,9 +0,0 @@
-﻿import React from 'react'
-import RemoteComponent from './RemoteComponent'
-
-export default function Assistant(){
-  return <RemoteComponent src="/ai/components/ai-assistant.html" onInit={() => {
-    try { if (window.IVSAssistant && typeof window.IVSAssistant.init === 'function') window.IVSAssistant.init(); else if (typeof IVSAssistant === 'function') window.IVSAssistant = new IVSAssistant(); } catch(e){}
-  }} />
-}
-
--- webapp/src/components/Footer.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import RemoteComponent from './RemoteComponent'
-
-export default function Footer(){
-  return <RemoteComponent src="/components/footer.html" />
-}
