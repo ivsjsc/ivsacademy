a0e6da67cf668c5f3735819ed0db3d0dbb0dbb2a
ivsjsc <ivscorp.vn@gmail.com>
2026-07-07 18:18:32 +0700

    commit

--- js/contact-config.js
+++ js/contact-config.js
@@ -5,7 +5,7 @@
  * Set CONTACT_FORM_ENDPOINT to the Google Apps Script Web App URL after deployment.
  */
 window.IVS_CONTACT_CONFIG = Object.freeze({
-  CONTACT_FORM_ENDPOINT: '',
+  CONTACT_FORM_ENDPOINT: 'https://formspree.io/f/xzdlwwvq',
   FALLBACK_EMAIL: 'nmi.triet@gmail.com',
   EMAIL_TO: 'nmi.triet@gmail.com'
 });
